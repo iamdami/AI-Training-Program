4e72dd28c75fe0c7f9b9f659987b6b72e14e0ed8
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-11-09 10:48:28 +0900

    Add files via upload

---
+++ Code/Day 1/HWPython01_03_Var01_이다미.py
@@ -0,0 +1,20 @@
+x = 'Dami'
+y = "Lee"
+print(type(x))
+print(type(y))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
---
+++ Code/Day 1/HWPython01_03_이다미.py
@@ -0,0 +1,14 @@
+x = 'Dami'
+y = "Lee"
+print(type(x))
+print(type(y))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
