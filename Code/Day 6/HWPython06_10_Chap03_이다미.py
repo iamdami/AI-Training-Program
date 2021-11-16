93b666c3272aed98a654cd5e7ce312aa888c39fc
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-11-16 19:00:27 +0900

    Update HWPython06_10_Chap03_이다미.py

--- Code/Day 6/HWPython06_10_Chap03_이다미.py
+++ Code/Day 6/HWPython06_10_Chap03_이다미.py
@@ -1,6 +1,5 @@
 """
 a = "Life is too short, you need python"
-
 if "wife" in a: print("wife")
 elif "python" in a and "you" not in a: print("python")
 elif "shirt" not in a: print("shirt")
@@ -8,21 +7,25 @@ elif "need" in a: print("need")
 else: print("none")
 """
 
-
-n=0
-while n>=1 and n<=1000 :
-	if n%3==0:
-		n+=1
-		print(sum(n))
-		break
-
+"""
+n = 1
+result = 0
+while n <= 1000:
+    if n % 3 == 0:
+        result += n
+    n += 1
+print(result)
+"""
 
 """
-n=1
-while n>6:
-	print('*'*n)
-	n+=1
-	break
+i = 1
+while i <= 5:
+    j = 1
+    while j <= i:
+        print('*', end=" ")
+        j += 1
+    print()
+    i += 1
 """
 
 """
@@ -36,9 +39,8 @@ a = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
 print(sum(a)/len(a))
 """
 
-"""
+
 n=0
 numbers = [1, 2, 3, 4, 5]
 result = [n*2 for n in numbers if n % 2 == 1]
 print(result)
-"""
