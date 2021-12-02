80a6f40926914c59966af77b1d3794351d041e76
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-12-02 14:10:21 +0900

    Update HWPython14_09_Recommend_이다미.py

--- Code/Day 14/HWPython14_09_Recommend_이다미.py
+++ Code/Day 14/HWPython14_09_Recommend_이다미.py
@@ -1,6 +1,20 @@
-from collections import Counter
+reader = ('케인', '손흥민', '홀란드', '음바페', '호날두', '반니', '케인', '반니', '카카', '드록바', '케인', '카카',
+          '손흥민', '홀란드', '음바페', '호날두', '메시', '케인', '카카', '델리알리', '케인', '손흥민', '외질', '외질')
 
-stuList = [민교,강경태, 고재호, 김태인, 한현기,재호,민교,재호,선웅,박연희,민교, 선웅, 
-강경태, 고재호, 김태인, 한현기, 이준혁,민교, 선웅,임재우,민교,강경태,경태,경태]
+Name = 0
+result = []
+resultSet = {}
 
-print(Counter(stuList))
+readerList = list(reader)
+
+for Name in readerList:
+    if len(Name) == 3:
+        result.append(Name[1:len(Name)])
+    elif len(Name) == 4:
+        result.append(Name[2:len(Name)])
+    else:
+        result.append(Name)
+print(result)
+
+resultSet = set(result)
+print(resultSet)
