b4e38ed0c6b30c7a69ace22591c575b3309982b8
Dami Lee <50016477+iamdami@users.noreply.github.com>
2022-05-20 17:48:28 +0900

    Update nlpTest.py

--- ML/NLP/nlpTest.py
+++ ML/NLP/nlpTest.py
@@ -2,6 +2,6 @@ from konlpy.tag import Okt
 t = Okt()
 
 
-print(t.nouns('자연어 처리 스타뚜~'))
-print(t.morphs('자연어 처리 스타뚜~'))
-print(t.pos('자연어 처리 스타뚜~'))
+print(t.nouns('자연어 처리 스타뚜~'))  # ['자연어', '처리', '스타', '뚜']
+print(t.morphs('자연어 처리 스타뚜~'))  # ['자연어', '처리', '스타', '뚜', '~']
+print(t.pos('자연어 처리 스타뚜~'))  # [('자연어', 'Noun'), ('처리', 'Noun'), ('스타', 'Noun'), ('뚜', 'Noun'), ('~', 'Punctuation')]
